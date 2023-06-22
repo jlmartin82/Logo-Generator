b75564cb35a9c1ef39d5dd5ff44bab4686ca062d
Justin <justin.martin@hsc.utah.edu>
2023-06-22 08:55:13 -0600

    added promts

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const { create } = require('svg-crowbar');
+
+inquirer 
+
+  .promt([
+    {
+      type: 'input',
+      name: 'text',
+      message: 'Enter up to three characters for the logo text:'
+      validate: function (input) {
+        return input.length <= 3 || 'Please enter three characters.';
+      }
+    },
+    {
+      type: 'input',
+      name: 'textColor',
+      message: 'Enter the text color (keyword or hexadecimal number):'
+    }
+  ])
+
+
+
+
+
+
+
+
+
+
+
+  
