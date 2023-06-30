bfe66a237223da1c2fdbbd350d6944d0a01ac5aa
Justin <justin.martin@hsc.utah.edu>
2023-06-29 21:46:21 -0600

    read me

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# Logo-Generator
-The application prompts the user to select a color and shape, provide text for the logo.
+# Logo Generator
+
+This is a simple script that generates a logo SVG file. The user is prompted to enter the text, text color, shape, and shape color for the logo. The script then writes the SVG file to a file called `logosvg.svg`.
+
+## Requirements
+
+* Node.js
+* Inquirer
+* Fs
+
+## Usage
+
+To generate a logo, run the following command:
+
+
+node logo.js
+
+
+The script will prompt you to enter the following information:
+
+* The text for the logo (up to three characters)
+* The text color (keyword)
+* The shape of the logo (circle, square, or triangle)
+* The color of the shape (keyword)
+
+Once you have entered all of the information, the script will generate the SVG file and save it to a file called `logosvg.svg`.
+
+## Example
+
+The following is an example of how to use the script:
+
+
+node logo.js
+
+Enter up to three characters for the logo text: TXT
+
+Enter the text color (keyword): red
+
+Select a shape for the logo: circle
+
+Enter the shape color (keyword): blue
+
+Generated logo.svg
+
+
+This will generate a logo SVG file with the text "TXT" in red, a blue circle, and a white background.
+
+## License
+
+This script is released under the MIT License.
+
--- index.js
+++ index.js
@@ -1,57 +1,52 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
-// shapes.js
+const fs = require("fs");
+const inquirer = require("inquirer");
+
 
 const shapes = {};
 
 module.exports = shapes;
 
-
 inquirer
   .prompt([
     {
-      type: 'input',
-      name: 'text',
-      message: 'Enter up to three characters for the logo text:',
+      type: "input",
+      name: "text",
+      message: "Enter up to three characters for the logo text:",
       validate: function (input) {
-        return input.length <= 3 || 'Please enter three characters.';
-      }
+        return input.length <= 3 || "Please enter three characters.";
+      },
     },
     {
-      type: 'input',
-      name: 'textColor',
-      message: 'Enter the text color (keyword or hexadecimal number):'
+      type: "input",
+      name: "textColor",
+      message: "Enter the text color (keyword):",
     },
     {
-      type: 'list',
-      name: 'shape',
-      message: 'Select a shape for the logo:',
-      choices: ['circle', 'square', 'triangle']
+      type: "list",
+      name: "shape",
+      message: "Select a shape for the logo:",
+      choices: ["circle", "square", "triangle"],
     },
     {
-      type: 'input',
-      name: 'shapeColor',
-      message: 'Enter the shape color (keyword or hexadecimal number):'
+      type: "input",
+      name: "shapeColor",
+      message: "Enter the shape color (keyword):",
     },
   ])
   .then(generateLogo)
   .catch(console.error);
 
-  function shapeSelected(svgShape, svgColor) {
-    if (svgShape === "circle") {
-      return `<circle cx="150" cy="100" r="100" fill="${svgColor}"/>`;
-    }
-    if (svgShape === "square") {
-      return `<rect width="300" height="100" style="fill:${svgColor};stroke-width:3;stroke:black;" />`;
-    }
-    if (svgShape === "triangle") {
-      return `<polygon points="150,30 270,170 30,170" style="fill:${svgColor};" />`;
-    }
+function shapeSelected(svgShape, svgColor) {
+  if (svgShape === "circle") {
+    return `<circle cx="150" cy="100" r="100" fill="${svgColor}"/>`;
   }
-  
-  
-  
-
+  if (svgShape === "square") {
+    return `<rect width="300" height="100" style="fill:${svgColor};stroke-width:3;stroke:black;" />`;
+  }
+  if (svgShape === "triangle") {
+    return `<polygon points="150,18 244,182 56,182" style="fill:${svgColor};" />`;
+  }
+}
 
 function generateLogo(answers) {
   const { text, textColor, shape, shapeColor } = answers;
@@ -59,27 +54,16 @@ function generateLogo(answers) {
   const svgMarkup = `
     <svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
       <rect width="100%" height="100%" fill="white" />
-      ${shapeSelected(shape,shapeColor)}
+      ${shapeSelected(shape, shapeColor)}
       <text x="50%" y="50%" fill="${textColor}" text-anchor="middle" dominant-baseline="middle" font-size="60">${text}</text>
     </svg>
   `;
 
-  fs.writeFile('logosvg.svg', svgMarkup, (err) => {
+  fs.writeFile("logosvg.svg", svgMarkup, (err) => {
     if (err) {
-      console.error('Failed to save the SVG:', err);
+      console.error("Failed to save the SVG:", err);
     } else {
-      console.log('Generated logo.svg');
+      console.log("Generated logo.svg");
     }
   });
 }
-
-
-
-
-
-
-
-
-
-
-
